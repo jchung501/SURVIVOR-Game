907b439cfa65419bc73c458b261b5405b2cac245
jchung501 <johnchung2011@gmail.com>
2022-01-22 16:00:38 -0600

    edited ternary operators for readibility, added conditionals to ternarys for eat, sleep

--- script.js
+++ script.js
@@ -24,12 +24,6 @@ let days = 0;
 //////////////////////////////////////////
 
 // Button Click Function
-
-// const showHide = () => {
-//     click = document.getElementById('')
-//     click.style.display === "none" ? (click.style.display = "block") : click.style.display = "none"
-// }
-
 const buttonClick = (evt) => {
     if (evt.currentTarget.id === 'campfire') {
         campFire();
@@ -45,7 +39,6 @@ const buttonClick = (evt) => {
         sleep();
     }
 }
-
 // Creates a new div, add class game-text to the div, create p element, appends to game-text-container
 const gameTextDOM = document.querySelector('#game-text-container')
 const createDiv = (gameText) => {
@@ -59,26 +52,30 @@ const createDiv = (gameText) => {
 // create campfire
 const campFire = () => {
     document.getElementById('game-text-container').innerHTML = ''
-    survivor.hasZippo === true && survivor.woodCount >= 1 ? (
-    survivor.woodCount -= 1,
-    survivor.health += 10,
-    updateInfo(),
-    createDiv(`You built a campfire. Gaining 10 health and using one piece of wood.`)) : (createDiv('You don\'t have enough wood!'))
+    survivor.hasZippo === true && survivor.woodCount >= 1 
+    ? (survivor.woodCount -= 1, survivor.health += 10, updateInfo(),
+    createDiv(`You built a campfire. Gaining 10 health and using one piece of wood.`))
+    : (createDiv('You don\'t have enough wood!'))
 }
 // create signal fire
 const signalFire = () => {
     document.getElementById('game-text-container').innerHTML = ''
-    survivor.hasZippo === true && survivor.woodCount >= 10 ? (survivor.woodCount -= 10, updateInfo(), createDiv(`You have built a signal fire. A helicopter was able to see your signal. You are saved! You have survived and won!!!`)) : (createDiv(`You don't have enough wood!`))
+    survivor.hasZippo === true && survivor.woodCount >= 10 
+    ? (survivor.woodCount -= 10, updateInfo(), createDiv(`You have built a signal fire. A helicopter was able to see your signal. You are saved! You have survived and won!!!`)) 
+    : (createDiv(`You don't have enough wood!`))
 }
 // cook
 const cook = () => {
     document.getElementById('game-text-container').innerHTML = '';
-    survivor.hasZippo === true && survivor.woodCount >= 1 && survivor.rabbitMeat >= 1 ? (survivor.woodCount -= 1, survivor.rabbitMeat -= 1, updateInfo(), createDiv(`You have cooked some rabbit meat. It has been added to your inventory. Each cooked rabbit meat adds 50 to your hunger level and 30 healthpoints.`)) : createDiv(`You don't have enough wood or rabbit meat to cook!`)
+    survivor.hasZippo === true && survivor.woodCount >= 1 && survivor.rabbitMeat >= 1 ? (survivor.woodCount -= 1, survivor.rabbitMeat -= 1, updateInfo(), createDiv(`You have cooked some rabbit meat. It has been added to your inventory. Each cooked rabbit meat adds 50 to your hunger level and 30 healthpoints.`)) 
+    : createDiv(`You don't have enough wood or rabbit meat to cook!`)
 }
 // eat
 const eat = () => {
     document.getElementById('game-text-container').innerHTML = '';
-    survivor.cookedRabbitMeat >= 1 ? (survivor.hunger += 50, survivor.health = 30, survivor.cookedRabbitMeat -= 1, updateInfo(), createDiv(`You have eaten a cooked rabbit meat. Your hunger level is now at ${survivor.hunger} and your health at ${survivor.health}.`)) : createDiv(`You do not have any cooked rabbit meat to eat!`)
+    survivor.cookedRabbitMeat >= 1 
+    ? (survivor.hunger += 50, survivor.health = 30, survivor.cookedRabbitMeat -= 1, updateInfo(), createDiv(`You have eaten a cooked rabbit meat. Your hunger level is now at ${survivor.hunger} and your health at ${survivor.health}.`)) 
+    : createDiv(`You do not have any cooked rabbit meat to eat!`)
 }
 // updates info within DOM
 const updateInfo = () => {
@@ -90,14 +87,13 @@ const updateInfo = () => {
 // sleep
 const sleep = () => {
     document.getElementById('game-text-container').innerHTML = '';
-    survivor.health += 20;
+    survivor.health >= 100 ? survivor.health = 100 : survivor.health += 20;
     survivor.hunger -= 20;
     survivor.woodCount -= 1;
     days += 1;
     updateInfo();
     createDiv(`You sleep and rest until next morning.`)
 }
-
 
 //////////////////////////////////////////
 ///////// DOM EVENT LISTENERS ////////////
